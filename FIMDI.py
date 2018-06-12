11b54258e72f969e33af78fdb30b6a312f96bf66
Chu Luo <mr.cluo29@gmail.com>
2018-06-12 21:10:18 +1000

    Create FIMDI.py

---
+++ FIMDI.py
@@ -0,0 +1,21 @@
+# FIMDI.py
+# Chu Luo
+
+# using feature importance for MDI
+
+import numpy as np
+
+def eva_FIMDI(X_train, X_test_m, Y_train, Y_test, column_id, label, X_test_c):
+    # X_test_m  has missing data already
+    # column_id is the feature missing data
+    # label is how missing data is marked
+    # X_test_c is complete X test set
+    print()
+
+def make_missing(column_id, label, X_test_c):
+    # to make a test set miss some data in a feature
+    print()
+
+def read_and_run(train_file, test_file, column_id, label):
+    # read train and test file from csv
+    print()
