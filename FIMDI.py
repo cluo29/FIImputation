b4f681331a4daf49f57eecdcd1985ec363929769
Chu Luo <mr.cluo29@gmail.com>
2018-06-20 16:19:46 +1000

    Update FIMDI.py

--- FIMDI.py
+++ FIMDI.py
@@ -32,13 +32,13 @@ class FIMDImputer():
         self.mdi = MDIOnline.MDImputer(X_train, column_id)
 
         # 3. train the targeted classifiers
-        # SVC(gamma=2, C=1)
+        #
         # http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
         # http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier
+        self.clf1 = SVC()
+        self.clf1.fit(X_train, Y_train)
+        # self.clf1.predict(X)
 
-
-def FI_impute():
-    print('TODO')
 
 def run_FIMDI(X_train, X_test_m, Y_train, Y_test, column_id, label, X_test_c):
     # X_test_m  has missing data already
@@ -65,18 +65,56 @@ def run_FIMDI(X_train, X_test_m, Y_train, Y_test, column_id, label, X_test_c):
         # one row is got, do basic MDI
         current_row = X_test_m[i, :]
 
-        print(current_row)
-
         # check if this row misses data
         if current_row[column_id] == label:
             # missing data
-            # do imputation
+            # do imputations
             mdi_list = FIMDI1.mdi.all_impute(current_row)
 
-            # do a prediction
+            FI_diff = []
 
-            # get FI
-            a=123
+            # do prediction for each imputation
+            for j in mdi_list:
+                impute_row = np.array(current_row)
+
+                impute_row[column_id] = j
+
+                impute_row = np.array([impute_row])
+
+                impute_pred = FIMDI1.clf1.predict(impute_row)
+
+                impute_pred = np.array([impute_pred])
+
+                # get FI of complete data
+
+                FI_complete = FIMDI1.detector.get_FI()
+
+                #print('FI_complete = ', FI_complete[column_id])
+
+                last_FI_column_id = FI_complete[column_id]
+
+                # get FI of impute
+                FI_list = FIMDI1.detector.measure_FI(impute_row, impute_pred)
+
+                #print('FI = ', FI_list[column_id])
+
+                FI_diff.append(abs(last_FI_column_id - FI_list[column_id]))
+
+            FI_diff_np = np.array(FI_diff)
+
+            min_FI_diff_index = np.argmin(FI_diff_np)
+
+            FI_impute_result = mdi_list[min_FI_diff_index]
+
+            print('all mdi = ', mdi_list)
+
+            print('FI_impute_result = ', FI_impute_result)
+
+            FI_impute_GT = X_test_c[i, column_id]
+
+            print('ground truth = ', FI_impute_GT)
+
+
         else:
             # complete row
             # update FI and last observation(basic imputation)
@@ -91,7 +129,9 @@ def run_FIMDI(X_train, X_test_m, Y_train, Y_test, column_id, label, X_test_c):
             measure_Y = np.array([[ground_truth]])
 
             FI1 = FIMDI1.detector.measure_FI(measure_X, measure_Y, 0)
-            print('FI1 = ', FI1)
+
+            FIMDI1.detector.set_FI(list(FI1.values()))
+
 
             #FI1 is a dictionary, turn it to list then use set_FI
 
@@ -112,7 +152,7 @@ def make_random_missing(column_id, label, X_test_c, rate_of_missing=0.1):
     # a = np.random.choice(5, 4, replace=False)
     # will get 0-4
     # unique ones! by using False
-    outputSet = X_test_c
+    outputSet = np.array(X_test_c)
 
     missing_count = int(np.floor(rate_of_missing * rows))
 
@@ -156,7 +196,7 @@ def read_and_run(train_file, test_file, column_id, label=-1, seed=0):
 
     #Y_test = Y_test.reshape(Y_test.shape[0], -1)
 
-    #print(X_test_c)
+    print('X_test_c', X_test_c)
     #print(Y_test)
 
     # now make missing data
